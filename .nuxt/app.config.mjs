ebff018b96d862175e481aaaf0420a7446c0d900
sho <miomini3715@gmail.com>
2023-06-24 22:40:22 +0900

    first

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from 'C:/nuxt/weather-project/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///C:/nuxt/weather-project/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
