c7236d1c91bb178b591ac33a23b56af01cf17c84
sho <miomini3715@gmail.com>
2023-06-24 22:28:50 +0900

    remanage .nuxt folder

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from 'C:/nuxt/weather-project/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/nuxt/weather-project/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
